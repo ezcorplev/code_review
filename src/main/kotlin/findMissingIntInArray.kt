d478ca58ce1f602b59c06f132c64b9a10c9c4c22
ezcorplev <ezcorplev@gmail.com>
2022-08-24 09:05:00 +0300

    fixed missing int in array algo

--- src/main/kotlin/findMissingIntInArray.kt
+++ src/main/kotlin/findMissingIntInArray.kt
@@ -5,42 +5,25 @@
 // Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3].
 // 2 is the missing number in the range since it does not appear in nums.
 
-
-
-
-
-
-//    val sortedNums = nums.sorted()
-//    val matchList = mutableListOf<Int>()
-//    val listSize = nums.maxOrNull()?.toInt() ?: "List is empty"
-//    for (i in 0..listSize as Int) {
-//        matchList.add(i)
-//
-//    }
-//    for (i in sortedNums) {
-//        if (sortedNums[i] == matchList[i]) {
-//
-//        }
-//    }
-//    return matchList[matchList.size-1]
-
 fun findMissingIntInArray(nums: List<Int>): Int? {
 
     if (nums.isEmpty()) {return null}
 
+    var numsSorted = nums.sorted()
     var missingNum: Int = 0
 
     for (i in 0 until nums.size) {
 
-        if (i != nums[i]) {
+        if (i != numsSorted[i]) {
             missingNum = i
+            break
         }
     }
     return missingNum
 }
 
 fun main() {
-    val nums = listOf(3, 0, 1)
+    val nums = listOf(0, 1, 5, 3, 2)
     val result = findMissingIntInArray(nums)
     println(result)
 }
